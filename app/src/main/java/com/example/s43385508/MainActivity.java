0368e043ddc5e3dcd61b07fb4af7a327ab663ba0
Ishmael Mafole <ishmaelmafole@gmail.com>
2022-08-13 14:20:00 +0200

    Perfectly running marathon calculator with Dark Mode on all activtites

--- app/src/main/java/com/example/s43385508/MainActivity.java
+++ app/src/main/java/com/example/s43385508/MainActivity.java
@@ -1,83 +1,144 @@
 package com.example.s43385508;
-//43385508
-
+/*Student Number: 43385508
+* Name: Ishmael Lehlohonolo Mafole
+* Purpose: a_marathon_app_that_allows_you_to_insert_your_time_for
+*          scores_enter_the_time_you_ran_below_then_press_submit.
+* The Application is developed in accordance to the specifications of ICT2612
+*/
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import com.google.android.material.button.MaterialButton;
+import android.widget.Spinner;
 
 
 public class MainActivity extends AppCompatActivity {
-  private  EditText diHours, diMinutes;
-    private  SwitchCompat dark_mode;
-  private  RelativeLayout background;
-
-
+  EditText diHours, diMinutes;
+  //SwitchCompat dark_mode;
+  RelativeLayout background;
+    private SwitchCompat dark_mode;
+    @SuppressLint("SetTextI18n")
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //EditText
+    protected void onCreate(
+            Bundle savedInstanceState)
+    {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+//locating dark mode switch via its ID
+    dark_mode = findViewById(R.id.theme);
+
+    //Using shared Preferences to store previous states
+        SharedPreferences sharedPreferences = getSharedPreferences(
+                "sharedPrefs", MODE_PRIVATE
+        );
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final boolean  isDarkModeOn = sharedPreferences.getBoolean(
+                "isDarkModeOn", false
+        );
+        //user reopens app
+        if (isDarkModeOn){
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES
+            );
+            dark_mode.setText("Enable Light Mode");
+        }
+        else
+        {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO);
+            dark_mode.setText("Switch to Dark Mode");
+        }
+//On clicking the button dark mode is activated or deactivated
+    dark_mode.setOnClickListener(
+            view -> {
+                if (isDarkModeOn) {
+                    AppCompatDelegate.setDefaultNightMode(
+                            AppCompatDelegate.MODE_NIGHT_NO
+                    );
+                    editor.putBoolean(
+                            "isDarkModeOn", false
+                    );
+                    editor.apply();
+                } else {
+                    //if dark mode is off
+                    AppCompatDelegate.setDefaultNightMode(
+                            AppCompatDelegate.MODE_NIGHT_YES
+                    );
+                    editor.putBoolean(
+                            "isDarkModeOn", true
+                    );
+                    editor.apply();
+                }
+
+            }
+    );
+
+
+
+        //EditText location via ID
         diHours = findViewById(R.id.hours);
         diMinutes = findViewById(R.id.minutes);
-        //Button
-        MaterialButton buttone = findViewById(R.id.button);
+        //Toggle Button location via ID
         dark_mode = findViewById(R.id.theme);
-        //
+        //BackgroundColor for Theme location via ID
         background = findViewById(R.id.layout);
 
-        //Fullscreen mode activated by default
+        //Button location via ID
+        MaterialButton buttone = findViewById(R.id.button);
+
+        //Fullscreen mode activated by default after setting Android Manifest file to transparent.
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-        dark_mode.setOnClickListener(view -> {
-
-            if (dark_mode.isChecked()) {
-                //when checked
-                background.setBackgroundResource(R.color.dark);
-            }
-                else
-                {
-                    //unchecked
-                    background.setBackgroundResource(R.color.day);
-            }
-        });
-
+        //Submit Button on-click listener using Lambda to minimize clutter
         //Button click listener
         buttone.setOnClickListener(v -> {
-
 
             //data from Edit texts
             String simplestDiHours = diHours.getText().toString();
             String simplestDiMinutes = diMinutes.getText().toString();
-
-
+    // using isEmpty and setError to validate input fields
             if (TextUtils.isEmpty(diHours.getText().toString()))
             {
                 diHours.setError("Please enter the Hours you Ran");
             }
-
+            else if( Integer.parseInt(diHours.getText().toString()) > 10 )
+            {
+                diHours.setError("Max hrs is 10");
+            }
             else if (TextUtils.isEmpty(diMinutes.getText().toString())) {
                 diMinutes.setError("Please enter the Minutes you Ran");
             }
-
-
-
+            else if( Integer.parseInt(diMinutes.getText().toString()) > 59)
+            {
+                diMinutes.setError("Max is 59");
+            }
+            else if (Integer.parseInt(diMinutes.getText().toString()) == 0 && Integer.parseInt(diHours.getText().toString()) == 0  )
+            {
+                diMinutes.setError("You did not run");
+            }
             else
             {
+                //I used intents to pass the MainActivity data to MainActivity2
                 Intent i = new Intent(MainActivity.this, MainActivity2.class);
                 i.putExtra("simplestDiMinutes", simplestDiMinutes);
                 i.putExtra("simplestDiHours", simplestDiHours);
                 startActivity(i);
                 finish();
             }
-        });
+        }
+        );
     }
 }
+
--- app/src/main/java/com/example/s43385508/MainActivity2.java
+++ app/src/main/java/com/example/s43385508/MainActivity2.java
@@ -1,19 +1,19 @@
 package com.example.s43385508;
-//43385508
-import androidx.appcompat.app.ActionBar;
+/*Student Number: 43385508
+ * Name: Ishmael Lehlohonolo Mafole
+ * Purpose: a_marathon_app_that_allows_you_to_insert_your_time_for
+ *          scores_enter_the_time_you_ran_below_then_press_submit.
+ * The Application is developed in accordance to the specifications of ICT2612
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -21,6 +21,9 @@ import java.text.MessageFormat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+ //spinner aka dropdown menu with medal names
+ private Spinner spinner;
+ private   final String[] paths = {"Gold_Medal", "Silver_Medal","Bronze_Medal"};
  TextView simplestResult1, simplestResult2;
  MaterialButton backButton, exitButton;
 
@@ -34,66 +37,62 @@ public class MainActivity2 extends AppCompatActivity {
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-        //buttons and textview are referred to below:
-
+        //buttons and textview are referred  to below located by IDs :
         simplestResult1 = findViewById(R.id.medalEarned);
         simplestResult2 = findViewById(R.id.averageResults);
 
+        //collecting input data from MainActivity to MainActivity2
         Intent intent = getIntent();
         String simplestDiHours = intent.getStringExtra("simplestDiHours");
         String simplestDiMinutes = intent.getStringExtra("simplestDiMinutes");
 
+        //declaration and conversion of strings passed from MainActivity.java and float time for accuracy
         int huure = Integer.parseInt(simplestDiHours);
         int minute = Integer.parseInt(simplestDiMinutes);
         int timeHours = huure * 60;
         float timeRan = timeHours + minute;
         float averagePerHour = (float) (timeRan/42.2);
 
+        //drawing images from their ID's before making them an array of images
         ImageView pictureGold = (ImageView) findViewById(R.id.imgGold);
         ImageView pictureSilver = (ImageView)findViewById(R.id.imgSilver);
         ImageView pictureBronze = (ImageView)findViewById(R.id.imgBronze);
 
+        //An Array of Images declared
+        ImageView[] images = {pictureGold, pictureSilver,pictureBronze };
+
+
         if (averagePerHour<11)
         {
-            pictureGold.setVisibility(View.VISIBLE);
+            images[0].setVisibility(View.VISIBLE); //an array with index 0 called to be visible
             simplestResult1.setText(MessageFormat.format("You have run: {0} minute(s) every Kilometer", averagePerHour));
             simplestResult2.setText("Woooow! You have earned a Gold Medals");
         }
         else if (averagePerHour<15)
         {
-            pictureSilver.setVisibility(View.VISIBLE);
-            simplestResult1.setText(MessageFormat.format("You have run: {0}minute(s) every Kilometer", averagePerHour));
+            images[1].setVisibility(View.VISIBLE); //an array with index 1 called to be visible
+            simplestResult1.setText(MessageFormat.format("You have run: {0} minute(s) every Kilometer", averagePerHour));
             simplestResult2.setText("Yeeey! You have earned a Silver Medals");
         }
         else
         {
-            pictureBronze.setVisibility(View.VISIBLE);
-            simplestResult1.setText(MessageFormat.format("You have run: {0}minute(s) every Kilometer", averagePerHour));
+            images[2].setVisibility(View.VISIBLE); //an array with index 2 called to be visible
+            simplestResult1.setText(MessageFormat.format("You have run: {0} minute(s) every Kilometer", averagePerHour));
             simplestResult2.setText("You have earned a Bronze Medals");
         }
 
-        //back Button
+        //back Button via ID
          backButton = findViewById(R.id.backArrow);
 
-        backButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view) {
-               Intent intent = new Intent(MainActivity2.this,MainActivity.class);
-               startActivity(intent);
-               finish();
-            }
+        backButton.setOnClickListener(view -> {
+           Intent intent1 = new Intent(MainActivity2.this,MainActivity.class);
+           startActivity(intent1);
+           finish();
         });
 
-        //exitButton
+        //exitButton using finishAffinity to kill the running app processes
         exitButton = findViewById(R.id.exit);
 
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                finishAffinity();
-            }
-        });
+        exitButton.setOnClickListener(view -> finishAffinity());
     }
 }
